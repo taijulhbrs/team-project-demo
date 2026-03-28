2b9f7019a65ce5a5f5b26881204116eaad8547a8
Taijul MAS <md.islam@smail.inf.h-brs.de>
2026-03-28 12:33:08 +0100

    New feature added called Calculator in the main.py file where user will give the first numer and then chose which operation and second number,finally will get the result as output

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 robot_name= input("What is your robot's name? ")
 print("Hello, " + robot_name + "! Welcome to the world of robotics!")
+
+#creating a new feature called calculator:
+def calculator():
+    print("Welcome to the calculator!")
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter the operator (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        result = num1 / num2
+    else:
+        print("Invalid operator!")
+        return
+
+    print("The result is:", result)
+
+# Call the calculator function
+calculator()
